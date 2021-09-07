3efa6634eb0735bdfd84a4385ebf34f77c739ffb
CasparBosch <74655948+CasparBosch@users.noreply.github.com>
2021-09-07 16:26:01 +0200

    Add files via upload

---
+++ controls.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class controls : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
